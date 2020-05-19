e2b32868926b35f5084e055fbc05adbfdb796d69
VoytechG <wojtek@Wojtek-Book.localdomain>
2020-05-19 11:26:59 +0200

    add doubler

--- unet/unet_basic/model_3d.py
+++ unet/unet_basic/model_3d.py
@@ -17,9 +17,9 @@ def conv_bnorm_relu(input, filters_no, kernel=3, padding="same"):
     return relu
 
 
-def build_down_step(input, filters_no, dropout=None):
+def build_down_step(input, filters_no, dropout=None, double_second=False):
     conv = conv_bnorm_relu(input, filters_no)
-    conv = conv_bnorm_relu(conv, filters_no)
+    conv = conv_bnorm_relu(conv, filters_no if not double_second else filters_no * 2)
 
     to_pool = conv
     if dropout:
@@ -30,10 +30,10 @@ def build_down_step(input, filters_no, dropout=None):
     return conv, pool
 
 
-def build_bottom_step(input, filters_no):
+def build_bottom_step(input, filters_no, double_second=False):
 
     conv = conv_bnorm_relu(input, filters_no)
-    conv = conv_bnorm_relu(conv, filters_no)
+    conv = conv_bnorm_relu(conv, filters_no if not double_second else filters_no * 2)
     conv = layers.Dropout(0.5)(conv)
 
     return conv
@@ -51,52 +51,86 @@ def build_up_step(input, conv_link, filters_no):
     return conv
 
 
-def build_output_step(input, input_size):
+def build_output_step(input, input_size, cat_ce=True):
     frames, height, width, _ = input_size
     conv = conv_bnorm_relu(input, 2, kernel=(1, 1, 1))
-    # conv = conv_bnorm_relu(conv, 2, kernel=(frames, 1, 1), padding="valid")
-    conv = layers.Conv3D(
-        2,
-        (frames, 1, 1),
-        padding="valid",
-        kernel_initializer="he_normal",
-        name="last_linear",
-    )(conv)
-    conv = layers.Reshape((height, width, 2))(conv)
+
+    if cat_ce:
+        conv = layers.Conv3D(
+            2,
+            (frames, 1, 1),
+            padding="valid",
+            kernel_initializer="he_normal",
+            name="last_linear",
+        )(conv)
+        conv = layers.Reshape((height, width, 2))(conv)
+
+    else:
+        conv = layers.Conv3D(
+            1,
+            (frames, 1, 1),
+            padding="valid",
+            kernel_initializer="he_normal",
+            name="last_linear",
+            activation="sigmoid",
+        )(conv)
+        conv = layers.Reshape((height, width, 1))(conv)
+
     return conv
 
 
-def unet3d_in_out(input_size, start_filters_no=32):
+def unet3d_in_out(input_size, start_filters_no=32, double_second=False, cat_ce=True):
     def fil(depth):
         return start_filters_no * 2 ** (depth - 1)
 
     inputs = layers.Input(input_size)
 
-    conv1, pool1 = build_down_step(inputs, fil(1))
-    conv2, pool2 = build_down_step(pool1, fil(2))
-    conv3, pool3 = build_down_step(pool2, fil(3), dropout=0.5)
+    conv1, pool1 = build_down_step(inputs, fil(1), double_second=double_second)
+    conv2, pool2 = build_down_step(pool1, fil(2), double_second=double_second)
+    conv3, pool3 = build_down_step(
+        pool2, fil(3), double_second=double_second, dropout=0.5
+    )
 
-    conv_bottom = build_bottom_step(pool3, fil(4))
+    conv_bottom = build_bottom_step(pool3, fil(4), double_second=double_second)
 
     convup3 = build_up_step(conv_bottom, conv3, fil(3))
     convup2 = build_up_step(convup3, conv2, fil(2))
     convup1 = build_up_step(convup2, conv1, fil(1))
 
-    convout = build_output_step(convup1, input_size)
+    convout = build_output_step(convup1, input_size, cat_ce=cat_ce)
 
     return inputs, convout
 
 
-def unet3d_simply(input_size, start_filters_no=32):
-    inputs, outputs = unet3d_in_out(input_size, start_filters_no=start_filters_no)
+def unet3d_simply(
+    input_size, start_filters_no=32, cat_ce=True, metrics=None, double_second=False
+):
+    inputs, outputs = unet3d_in_out(
+        input_size,
+        start_filters_no=start_filters_no,
+        cat_ce=cat_ce,
+        double_second=double_second,
+    )
 
     model = keras.Model(inputs=inputs, outputs=outputs)
 
-    model.compile(
-        optimizer=keras.optimizers.Adam(lr=1e-4),
-        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
-    )
+    opt = keras.optimizers.Adam(lr=1e-4)
+
+    if cat_ce:
+        if metrics is None:
+            metrics = ["accuracy"]
+        model.compile(
+            optimizer=opt,
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=metrics,
+        )
+
+    else:
+        if metrics is None:
+            metrics = ["accuracy", keras.metrics.Precision(), keras.metrics.Recall()]
+        model.compile(
+            optimizer=opt, loss=keras.losses.BinaryCrossentropy(), metrics=metrics,
+        )
 
     return model
 
